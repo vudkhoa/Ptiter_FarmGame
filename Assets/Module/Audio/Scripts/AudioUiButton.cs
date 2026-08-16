4e85dab3596ea20b998e849010d9ae20c4373f3d
tanvmt <163519945+tanvmt@users.noreply.github.com>
2026-08-16 22:48:32 +0700

    refactor: use component-driven button feedback

--- Assets/Module/Audio/Scripts/AudioUiButton.cs
+++ Assets/Module/Audio/Scripts/AudioUiButton.cs
@@ -1,44 +1,24 @@
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
-using VContainer;
 
 namespace Core.Module.Audio
 {
     [RequireComponent(typeof(Button))]
-    public sealed class AudioUiButton : MonoBehaviour
+    public sealed class AudioUiButton : MonoBehaviour, IPointerClickHandler
     {
         [SerializeField, Range(0f, 1f)] private float _volume = 1f;
 
         private Button _button;
-        private IAudioService _audio;
-        private AudioCatalogSO _catalog;
-
-        [Inject]
-        public void Construct(
-            IAudioService audio,
-            AudioCatalogSO catalog)
-        {
-            _audio = audio;
-            _catalog = catalog;
-        }
-
         private void Awake() => _button = GetComponent<Button>();
 
-        private void OnEnable()
+        public void OnPointerClick(PointerEventData eventData)
         {
-            if (_button == null) _button = GetComponent<Button>();
-            _button.onClick.AddListener(Play);
-        }
-
-        private void OnDisable()
-        {
-            if (_button != null) _button.onClick.RemoveListener(Play);
-        }
-
-        public void Play()
-        {
-            if (_audio != null && _catalog != null)
-                _audio.PlaySfx(_catalog.ButtonClick, _volume);
+            if (eventData.button != PointerEventData.InputButton.Left) return;
+            if (_button.IsInteractable())
+                AudioUiFeedback.PlayClick(_volume);
+            else
+                AudioUiFeedback.PlayError();
         }
     }
 }
